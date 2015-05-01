f5a9f30814a821803e0895b102de38e56bf2e39a
Guillaume Lemay <guilemay@gmail.com>
2015-05-01 15:32:20 -0400

    Completed basic challenge

--- 212/rovarspraket.py
+++ 212/rovarspraket.py
@@ -1,1 +1,13 @@
-print("rovarspraket")
+output = ""
+consonents = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z']
+
+def enc(c):
+	l = c.lower()
+	if l in consonents:
+		return c + 'o' + l
+	return c
+
+for c in input():
+	output += enc(c)
+	
+print(output)
